73d8cc1e48fea9c05e724e61eddf2bbef239a87e
Rémi KOEBEL <rafael.koebel@gmail.com>
2026-01-11 16:35:01 +0100

    Création de l'entité User pour l'administration

--- src/Controller/Admin/AdminDashboardController.php
+++ src/Controller/Admin/AdminDashboardController.php
@@ -33,7 +33,7 @@ class AdminDashboardController extends AbstractDashboardController
         // Option 3. You can render some custom template to display a proper dashboard with widgets, etc.
         // (tip: it's easier if your template extends from @EasyAdmin/page/content.html.twig)
         //
-        // return $this->render('some/path/my-dashboard.html.twig');
+         return $this->render('admin/dashboard.html.twig');
     }
 
     public function configureDashboard(): Dashboard
