293ee5ec6d1a4a5f68805e1f0f63f262c88ed1ca
Rémi KOEBEL <rafael.koebel@gmail.com>
2026-03-02 11:08:23 +0100

    Ajout textures CSS & JS

--- public/js/main.js
+++ public/js/main.js
@@ -20,3 +20,47 @@ document.addEventListener('DOMContentLoaded', () => {
 if (window.location.pathname === "/contact") {
     document.getElementById("contact").style.display = "none";
   }
+// Halo respire
+  document.addEventListener("DOMContentLoaded", () => {
+    const container = document.getElementById("bg-animation");
+
+    function createBlob(color, top, left) {
+        const blob = document.createElement("div");
+        blob.classList.add("organic-blob");
+        blob.style.background = color;
+        blob.style.top = top;
+        blob.style.left = left;
+        container.appendChild(blob);
+    }
+
+    createBlob("rgba(38, 143, 125, 0.62)", "10%", "70%");
+    createBlob("rgba(131, 42, 173, 0.53)", "60%", "10%");
+});
+
+//particules
+document.addEventListener("DOMContentLoaded", () => {
+    const container = document.getElementById("bg-animation");
+
+    for (let i = 0; i < 20; i++) {
+        const p = document.createElement("div");
+        p.classList.add("particle");
+
+        p.style.top = Math.random() * 100 + "%";
+        p.style.left = Math.random() * 100 + "%";
+
+        p.animate(
+            [
+                { transform: "translateY(0px)" },
+                { transform: "translateY(-40px)" }
+            ],
+            {
+                duration: 8000 + Math.random() * 5000,
+                iterations: Infinity,
+                direction: "alternate",
+                easing: "ease-in-out"
+            }
+        );
+
+        container.appendChild(p);
+    }
+});
